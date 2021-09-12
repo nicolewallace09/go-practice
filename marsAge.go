55444c5e15169ac5d8b0c1a2135e84b87425f0de
nicolewallace09 <nicole.elisaw@gmail.com>
2021-09-12 16:13:55 -0700

    added flytoplanet

---
+++ marsAge.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+// Update marsYear so that it takes earthYears
+// As a parameter
+func computeMarsYears(earthYears int) int {  
+  earthDays := earthYears * 365
+  marsYears := earthDays / 687
+  return marsYears
+}
+
+func main() {
+  myAge := 25
+  
+  myMartianAge := computeMarsYears(myAge)
+  fmt.Println(myMartianAge)
+}
